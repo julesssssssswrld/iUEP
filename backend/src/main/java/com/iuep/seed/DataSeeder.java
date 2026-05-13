8dc9b7e57233b660844bd953eb0c15fc0be35098
julesssssssswrld <tomacasjulesiancajandab@gmail.com>
2026-05-13 14:03:53 +0800

    finalize sample data and edited about page

--- backend/src/main/java/com/iuep/seed/DataSeeder.java
+++ backend/src/main/java/com/iuep/seed/DataSeeder.java
@@ -43,10 +43,10 @@ public class DataSeeder implements CommandLineRunner {
         log.info("[Seeder] Seeding demo users...");
 
         seedUser("240475", "Jules Ian", "Cajandab", "Tomacas", "BSIT", "2", "C", "2006-06-08", "tomacasjulesiancajandab@gmail.com");
-        seedUser("235828", "Kent Jeanne", "Saradogan", "De Leon", "BSIT", "2", "C", "2000-11-21", "Lampake000@gmail.com");
-        seedUser("244556", "Keniel Drew", "D", "De Asis", "BSIT", "2", "C", null, "kenielddeasis@gmail.com");
-        seedUser("244530", "Jovan", "Pabia", "Atencio", "BSIT", "2", "C", "2006-02-05", "Jovanatencio17@gmail.com");
-        seedUser("240456", "Jose Manuel", "Morado", "Cardeno", "BSIT", "2", "C", null, null);
+        seedUser("235828", "Kent Jeanne", "Saradogan", "De Leon", "BSIT", "2", "C", "2000-11-21", "lampake000@gmail.com");
+        seedUser("244556", "Keniel Drew", "Dimaculangan", "De Asis", "BSIT", "2", "C", "2005-12-20", "kenielddeasis@gmail.com");
+        seedUser("244530", "Jovan", "Pabia", "Atencio", "BSIT", "2", "C", "2006-02-05", "jovanatencio17@gmail.com");
+        seedUser("240456", "Jose Manuel", "Morado", "Cardeno", "BSIT", "2", "C", "2006-09-23", "jmcardeno706@gmail.com");
 
         // Seed admin alongside users
         seedAdmin();
